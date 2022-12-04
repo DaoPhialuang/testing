dcded8067af522a25559ea298056afb26f62b6dd
DaoPhialuang <92082938+DaoPhialuang@users.noreply.github.com>
2022-12-04 16:15:18 -0600

    insert_sort

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("testing")
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i -1
+        while j>=0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+data = [-2,10,2,5,30,1]
+insertion_sort(data)
+print( data)
